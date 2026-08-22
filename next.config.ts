a769fc92322816309b6f9de2632aa30cc3b59b7e
shashank chauhan <shashankchauhan8577@gmail.com>
2026-08-22 14:34:08 +0530

    fixed deployment errors

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "firebase", "jose", "jwks-rsa"],
 };
 
 export default nextConfig;
